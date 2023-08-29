c6b5016c9984dc5f867985b5ab65200d91881e68
Misha Kukarkin <wardenclock@gmail.com>
2023-08-29 21:04:26 +0500

    feat: added sidebar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import {React} from 'react';
 import Navigation from '../Navigation/Navigation';
+import Profile from '../Profile/Profile';
 
 const Header = ({ sourceMain = false, loggedIn = false, handleIconClick, handleMenuClick }) => {
   return (
@@ -24,14 +25,7 @@ const Header = ({ sourceMain = false, loggedIn = false, handleIconClick, handleM
       }
       {loggedIn &&
         <div className='header__profile-wrapper'>
-          <div className='header__profile'>
-            <h2 className='header__title'>Аккаунт</h2>
-            <span className='header__icon'>
-              <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fillRule="evenodd" clipRule="evenodd" d="M7.42985 7.96781C8.79169 7.40572 9.7501 6.06478 9.7501 4.5C9.7501 2.42893 8.07117 0.75 6.0001 0.75C3.92903 0.75 2.2501 2.42893 2.2501 4.5C2.2501 6.06473 3.20845 7.40563 4.57021 7.96775C3.17555 8.19993 1.89263 8.76594 0.80835 9.58058L2.1899 11.4194C3.25119 10.6221 4.56874 10.15 5.99986 10.15C7.43098 10.15 8.74852 10.6221 9.80981 11.4194L11.1914 9.58058C10.1072 8.76601 8.82438 8.20003 7.42985 7.96781Z" fill="white"/>
-              </svg> 
-            </span>
-          </div>
+          <Profile profileName='Аккаунт'/>
           <button className='header__menu-button' onClick={handleMenuClick}>
             <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path fillRule="evenodd" clipRule="evenodd" d="M36 14L8 14V11L36 11V14Z" fill="white"/>
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,13 +1,28 @@
-import React from "react";
+import { React, useState } from "react";
 import Header from "../Header/Header";
+import NavTab from "../NavTab/NavTab";
 
 const Movies = ({loggedIn, handleIconClick}) => {
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
-    <Header
-      sourceMain={false}
-      loggedIn={loggedIn}
-      handleIconClick={handleIconClick}
-    />
+    <>
+      <Header
+        sourceMain={false}
+        loggedIn={loggedIn}
+        handleIconClick={handleIconClick}
+        handleMenuClick={toggleSidebar}
+      />
+      <NavTab
+        opened={sidebarOpen}
+        handleClose={toggleSidebar}
+      />
+    </>
   );
 }
 

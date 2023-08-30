88ab10ca4ce82365ea8b05df6ad12a4cb1155399
Misha Kukarkin <wardenclock@gmail.com>
2023-08-30 12:36:52 +0500

    feat: added promo component to the about page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,17 @@
 import {React} from 'react';
 import Header from '../Header/Header';
+import Promo from '../Promo/Promo';
 
 const Main = ({ loggedIn, handleIconClick }) => {
   return (
+    <>
     <Header
       sourceMain={true}
       loggedIn={loggedIn}
       handleIconClick={handleIconClick}
     />
+    <Promo/>
+    </>
   );
 }
 

031baea0ab0090d00055713766bc4e4d0c8ac3be
Misha Kukarkin <wardenclock@gmail.com>
2023-09-07 13:10:57 +0500

    feat: added default layout for the card list

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,9 +1,34 @@
 import React from "react";
 
-const MoviesCard = () => {
+const MoviesCard = ({isMain = true, isLiked = false, title, image, duration}) => {
+  const imageAlt = `Постер фильма: ${title}`;
+  const fillColor = isLiked ? '#EE3465' : 'white'
   return (
     <div className="card">
-
+      <img className="card__image" src={image} alt={imageAlt}></img>
+      <div className="card__title-wrapper">
+        <h2 className="card__title">{title}</h2>
+        {isMain &&
+          <button className="card__button">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect width="24" height="24" rx="12" fill="#313131"/>
+              <path d="M14.2727 8C13.2727 8 12.5455 8.52308 12 9.08974C11.4545 8.56667 10.7273 8 9.72727 8C8.13636 8 7 9.2641 7 10.8333C7 11.6179 7.31818 12.3154 7.90909 12.7949L12 16.5L16.0909 12.7949C16.6364 12.2718 17 11.6179 17 10.8333C17 9.2641 15.8636 8 14.2727 8Z" fill={fillColor}/>
+            </svg>
+          </button>
+        }
+        {!isMain &&
+          <button className="card__button">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect width="24" height="24" rx="12" fill="#313131"/> 
+              <path fillRule="evenodd" clipRule="evenodd" d="M4 5.06055L6.65156 7.71211L7.71222 6.65145L5.06066 3.99989L7.71211 1.34844L6.65145 0.287781L4 2.93923L1.34826 0.287484L0.287598 1.34814L2.93934 3.99989L0.287484 6.65174L1.34814 7.7124L4 5.06055Z" fill="white"/>
+            </svg> 
+          </button>
+        }
+      </div>
+      <div className="card__wrapper">
+        <span className="card__line"></span>
+        <h3 className="card__duration">{duration}</h3>
+      </div>
     </div>
   );
 }
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,9 +1,157 @@
 import React from "react";
+import MoviesCard from "../MoviesCard/MoviesCard";
+import image1 from "../../images/films1.png"
+import image2 from "../../images/flims2.png"
+import image3 from "../../images/flims3.png"
+import image4 from "../../images/flims4.png"
+import image5 from "../../images/flims5.png"
+import image6 from "../../images/flims6.png"
+import image7 from "../../images/flims7.png"
+import image8 from "../../images/flims8.png"
+import image9 from "../../images/flims9.png"
+import image10 from "../../images/flims10.png"
+import image11 from "../../images/flims11.png"
+import image12 from "../../images/flims12.png"
+import image13 from "../../images/flims13.png"
+import image14 from "../../images/flims14.png"
+import image15 from "../../images/flims15.png"
+import image16 from "../../images/flims16.png"
 
 const MoviesCardList = () => {
+  const title1 = "33 слова о дизайне";
+  const title2 = "Киноальманах «100 лет дизайна»";
+  const title3 = "В погоне за Бенкси";
+  const title4 = "Баския: Взрыв реальности";
+  const title5 = "Бег это свобода";
+  const title6 = "Книготорговцы";
+  const title7 = "Когда я думаю о Германии ночью";
+  const title8 = "Gimme Danger: История Игги и The Stooges";
+  const title9 = "Дженис: Маленькая девочка грустит";
+  const title10 = "Соберись перед прыжком";
+  const title11 = "Пи Джей Харви: A dog called money";
+  const title12 = "По волнам: Искусство звука в кино";
+  const title13 = "Рудбой";
+  const title14 = "Скейт — кухня";
+  const title15 = "Война искусств";
+  const title16 = "Зона";
+  const duration = "1ч 42м"
   return (
     <div className="list">
-      
+      <div className="list__wrapper">
+        <MoviesCard
+          isMain={true}
+          isLiked={false}
+          title={title1}
+          image={image1}
+          duration={duration}
+        />
+        <MoviesCard
+          isMain={true}
+          isLiked={false}
+          title={title2}
+          image={image2}
+          duration={duration}
+        />
+        <MoviesCard
+          isMain={true}
+          isLiked={false}
+          title={title3}
+          image={image3}
+          duration={duration}
+        />
+        <MoviesCard
+          isMain={true}
+          isLiked={false}
+          title={title4}
+          image={image4}
+          duration={duration}
+        />
+        <MoviesCard
+          isMain={true}
+          isLiked={false}
+          title={title5}
+          image={image5}
+          duration={duration}
+        />
+        <MoviesCard
+          isMain={true}
+          isLiked={false}
+          title={title6}
+          image={image6}
+          duration={duration}
+        />
+        <MoviesCard
+          isMain={true}
+          isLiked={false}
+          title={title7}
+          image={image7}
+          duration={duration}
+        />
+        <MoviesCard
+          isMain={true}
+          isLiked={false}
+          title={title8}
+          image={image8}
+          duration={duration}
+        />
+        <MoviesCard
+          isMain={true}
+          isLiked={false}
+          title={title9}
+          image={image9}
+          duration={duration}
+        />
+        <MoviesCard
+          isMain={true}
+          isLiked={false}
+          title={title10}
+          image={image10}
+          duration={duration}
+        />
+        <MoviesCard
+          isMain={true}
+          isLiked={false}
+          title={title11}
+          image={image11}
+          duration={duration}
+        />
+        <MoviesCard
+          isMain={true}
+          isLiked={false}
+          title={title12}
+          image={image12}
+          duration={duration}
+        />
+        <MoviesCard
+          isMain={true}
+          isLiked={false}
+          title={title13}
+          image={image13}
+          duration={duration}
+        />
+        <MoviesCard
+          isMain={true}
+          isLiked={false}
+          title={title14}
+          image={image14}
+          duration={duration}
+        />
+        <MoviesCard
+          isMain={true}
+          isLiked={false}
+          title={title15}
+          image={image15}
+          duration={duration}
+        />
+        <MoviesCard
+          isMain={true}
+          isLiked={false}
+          title={title16}
+          image={image16}
+          duration={duration}
+        />
+      </div>
+      <button className="list__button">Еще</button>
     </div>
   );
 }

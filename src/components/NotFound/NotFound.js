04a0f4e7a47364fae11330a39e1661ff143147b0
Wardenclock1759 <wardenclock@gmail.com>
2023-09-11 20:14:24 +0500

    refactor: moved footer and header from all pages to the main rendering component

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,7 +1,10 @@
-import React from "react";
+import {React, useEffect} from "react";
 import { useNavigate } from 'react-router-dom';
 
-const NotFound = () => {
+const NotFound = ({toggleShowFooter}) => {
+  useEffect(() => {
+    toggleShowFooter(false);
+  }, [])
 
   const navigate = useNavigate();
   
